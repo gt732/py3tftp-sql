95491552f4faa9464db129b6e34d0fb62aa93d3a
gt <geuryt@yahoo.com>
2023-03-07 15:41:28 -0500

    changed the functionality to write to postgres instead of create file

--- py3tftp/file_io.py
+++ py3tftp/file_io.py
@@ -8,7 +8,7 @@ def sanitize_fname(fname):
     Ensures that fname is a path under the current working directory.
     """
     # Remove root (/) and parent (..) directory references.
-    path = os.fsdecode(fname).lstrip('./')
+    path = os.fsdecode(fname).lstrip("./")
     abs_path = Path.cwd() / path
 
     # Verify that the formed path is under the current working directory.
@@ -24,50 +24,6 @@ def sanitize_fname(fname):
     return abs_path
 
 
-class FileReader(object):
-    """
-    A wrapper around a regular file that implements:
-    - read_chunk - for closing the file when bytes read is
-      less than chunk_size.
-    - finished - for easier notifications
-    interfaces.
-    When it goes out of scope, it ensures the file is closed.
-    """
-
-    def __init__(self, fname, chunk_size=0, mode=None):
-        self._f = None
-        self.fname = sanitize_fname(fname)
-        self.chunk_size = chunk_size
-        self._f = self._open_file()
-        self.finished = False
-
-        if mode == b'netascii':
-            self._f = Netascii(self._f)
-
-    def _open_file(self):
-        return self.fname.open('rb')
-
-    def file_size(self):
-        return self.fname.stat().st_size
-
-    def read_chunk(self, size=None):
-        size = size or self.chunk_size
-        if self.finished:
-            return b''
-
-        data = self._f.read(size)
-
-        if not data or (size > 0 and len(data) < size):
-            self._f.close()
-            self.finished = True
-
-        return data
-
-    def __del__(self):
-        if self._f and not self._f.closed:
-            self._f.close()
-
-
 class FileWriter(object):
     """
     Wrapper around a regular file that implements:
@@ -75,17 +31,14 @@ class FileWriter(object):
       is less than chunk_size.
     When it goes out of scope, it ensures the file is closed.
     """
+
     def __init__(self, fname, chunk_size, mode=None):
         self._f = None
         self.fname = sanitize_fname(fname)
         self.chunk_size = chunk_size
-        self._f = self._open_file()
 
-        if mode == b'netascii':
+        if mode == b"netascii":
             self._f = Netascii(self._f)
-
-    def _open_file(self):
-        return self.fname.open('xb')
 
     def _flush(self):
         if self._f:
--- py3tftp/protocols.py
+++ py3tftp/protocols.py
@@ -5,26 +5,32 @@ from py3tftp import file_io
 from py3tftp import tftp_parsing
 from py3tftp.exceptions import ProtocolException
 from py3tftp.tftp_packet import TFTPPacketFactory
-
+from py3tftp.database import connect_database
+import re
+from rich import print
 
 logger = logging.getLogger(__name__)
 
 
 class BaseTFTPProtocol(asyncio.DatagramProtocol):
     supported_opts = {
-        b'blksize': tftp_parsing.blksize_parser,
-        b'timeout': tftp_parsing.timeout_parser,
-        b'tsize': tftp_parsing.tsize_parser,
-        b'windowsize': tftp_parsing.windowsize_parser,
+        b"blksize": tftp_parsing.blksize_parser,
+        b"timeout": tftp_parsing.timeout_parser,
+        b"tsize": tftp_parsing.tsize_parser,
+        b"windowsize": tftp_parsing.windowsize_parser,
     }
 
-    default_opts = {b'timeout': 0.5, b'conn_timeout': 5.0, b'blksize': 512,
-                    b'windowsize': 1}
+    default_opts = {
+        b"timeout": 0.5,
+        b"conn_timeout": 5.0,
+        b"blksize": 512,
+        b"windowsize": 1,
+    }
 
     def __init__(self, packet, file_handler_cls, remote_addr, extra_opts=None):
         self.packet_factory = TFTPPacketFactory(
-            supported_opts=self.supported_opts,
-            default_opts=self.default_opts)
+            supported_opts=self.supported_opts, default_opts=self.default_opts
+        )
 
         self.remote_addr = remote_addr
         self.packet = self.packet_factory.from_bytes(packet)
@@ -67,31 +73,31 @@ class BaseTFTPProtocol(asyncio.DatagramProtocol):
         access the requested file - and handles possible file errors - as well
         as handling option negotiation (if applicable).
         """
-        logger.debug('Initializing file transfer to {addr}'.format(
-            addr=self.remote_addr))
+        logger.debug(
+            "Initializing file transfer to {addr}".format(addr=self.remote_addr)
+        )
         try:
             self.set_proto_attributes()
             self.initialize_transfer()
 
             if self.r_opts:
                 self.counter = 0
-                pkt = self.packet_factory.create_packet('OCK',
-                                                        r_opts=self.r_opts)
+                pkt = self.packet_factory.create_packet("OCK", r_opts=self.r_opts)
             else:
                 pkt = self.next_datagram()
         except FileExistsError:
-            logger.error('"{}" already exists! Cannot overwrite'.format(
-                self.filename))
+            logger.error('"{}" already exists! Cannot overwrite'.format(self.filename))
             pkt = self.packet_factory.err_file_exists()
         except PermissionError:
-            logger.error('Insufficient permissions to operate on "{}"'.format(
-                self.filename))
+            logger.error(
+                'Insufficient permissions to operate on "{}"'.format(self.filename)
+            )
             pkt = self.packet_factory.err_access_violation()
         except FileNotFoundError:
             logger.error('File "{}" does not exist!'.format(self.filename))
             pkt = self.packet_factory.err_file_not_found()
 
-        logger.debug('opening pkt: {}'.format(pkt))
+        logger.debug("opening pkt: {}".format(pkt))
         self.send_opening_packet(pkt.to_bytes())
 
         if pkt.is_err():
@@ -106,8 +112,7 @@ class BaseTFTPProtocol(asyncio.DatagramProtocol):
         self.filename = self.packet.fname
         self.r_opts = self.packet.r_opts
         self.opts = {**self.default_opts, **self.extra_opts, **self.r_opts}
-        logger.debug(
-            'Set protocol attributes as {attrs}'.format(attrs=self.opts))
+        logger.debug("Set protocol attributes as {attrs}".format(attrs=self.opts))
 
     def connection_lost(self, exc):
         """
@@ -117,11 +122,12 @@ class BaseTFTPProtocol(asyncio.DatagramProtocol):
         self.conn_reset()
         if exc:
             logger.error(
-                'Error on connection lost: {0}.\nTraceback: {1}'.format(
-                    exc, exc.__traceback__))
+                "Error on connection lost: {0}.\nTraceback: {1}".format(
+                    exc, exc.__traceback__
+                )
+            )
         else:
-            logger.info('Connection to {0}:{1} terminated'.format(
-                *self.remote_addr))
+            logger.info("Connection to {0}:{1} terminated".format(*self.remote_addr))
 
     def error_received(self, exc):
         """
@@ -132,9 +138,11 @@ class BaseTFTPProtocol(asyncio.DatagramProtocol):
         self.conn_reset()
         self.transport.close()
         logger.error(
-            ('Error receiving packet from {0}: {1}. '
-             'Transfer of "{2}" aborted.\nTraceback: {3}').format(
-                 self.remote_addr, exc, self.filename, exc.__traceback__))
+            (
+                "Error receiving packet from {0}: {1}. "
+                'Transfer of "{2}" aborted.\nTraceback: {3}'
+            ).format(self.remote_addr, exc, self.filename, exc.__traceback__)
+        )
 
     def send_opening_packet(self, packet):
         """
@@ -142,7 +150,8 @@ class BaseTFTPProtocol(asyncio.DatagramProtocol):
         """
         self.reply_to_client(packet)
         self.h_timeout = asyncio.get_event_loop().call_later(
-            self.opts[b'conn_timeout'], self.conn_timeout)
+            self.opts[b"conn_timeout"], self.conn_timeout
+        )
 
     def reply_to_client(self, packet):
         """
@@ -151,8 +160,9 @@ class BaseTFTPProtocol(asyncio.DatagramProtocol):
         """
         self.transport.sendto(packet, self.remote_addr)
         self.retransmit = asyncio.get_event_loop().call_later(
-            self.opts[b'timeout'], self.reply_to_client, packet)
-        if self.opts[b'windowsize'] > 1:
+            self.opts[b"timeout"], self.reply_to_client, packet
+        )
+        if self.opts[b"windowsize"] > 1:
             self.retransmits.append(self.retransmit)
 
     def handle_err_pkt(self):
@@ -160,9 +170,11 @@ class BaseTFTPProtocol(asyncio.DatagramProtocol):
         Cleans up connection after sending a courtesy error packet
         to offending client.
         """
-        logger.info(('Closing connection to {0} due to error. '
-                     '"{1}" Not transmitted.').format(self.remote_addr,
-                                                      self.filename))
+        logger.info(
+            (
+                "Closing connection to {0} due to error. " '"{1}" Not transmitted.'
+            ).format(self.remote_addr, self.filename)
+        )
         self.conn_reset()
         asyncio.get_event_loop().call_soon(self.transport.close)
 
@@ -170,7 +182,7 @@ class BaseTFTPProtocol(asyncio.DatagramProtocol):
         """
         Stops the message retry loop.
         """
-        if self.opts[b'windowsize'] > 1:
+        if self.opts[b"windowsize"] > 1:
             [retransmit.cancel() for retransmit in self.retransmits]
             self.retransmits = []
         else:
@@ -192,7 +204,9 @@ class BaseTFTPProtocol(asyncio.DatagramProtocol):
 
         logger.error(
             'Connection to {0} timed out, "{1}" not transfered'.format(
-                self.remote_addr, self.filename))
+                self.remote_addr, self.filename
+            )
+        )
         self.retransmit_reset()
         self.transport.close()
 
@@ -203,7 +217,8 @@ class BaseTFTPProtocol(asyncio.DatagramProtocol):
 
         self.conn_reset()
         self.h_timeout = asyncio.get_event_loop().call_later(
-            self.opts[b'conn_timeout'], self.conn_timeout)
+            self.opts[b"conn_timeout"], self.conn_timeout
+        )
 
     def is_correct_tid(self, addr):
         """
@@ -215,8 +230,10 @@ class BaseTFTPProtocol(asyncio.DatagramProtocol):
             return True
         else:
             logger.info(
-                'Unknown transfer id: expected {0}, got {1} instead.'.format(
-                    self.remote_addr, addr))
+                "Unknown transfer id: expected {0}, got {1} instead.".format(
+                    self.remote_addr, addr
+                )
+            )
             err_response = self.packet_factory.err_unknown_tid()
             self.transport.sendto(err_response.to_bytes(), addr)
             return False
@@ -225,146 +242,64 @@ class BaseTFTPProtocol(asyncio.DatagramProtocol):
 class WRQProtocol(BaseTFTPProtocol):
     def __init__(self, wrq, file_handler_cls, addr, opts):
         super().__init__(wrq, file_handler_cls, addr, opts)
-        logger.info('Initiating WRQProtocol with {0}'.format(
-            self.remote_addr))
-
+        logger.info("Initiating WRQProtocol with {0}".format(self.remote_addr))
+        self.data = b""  # Initialize empty byte string to hold incoming data
+        
     def next_datagram(self):
         """
         Builds an acknowledgement of a received data packet.
         """
-        return self.packet_factory.create_packet(pkt_type='ACK',
-                                                 block_no=self.counter)
+        return self.packet_factory.create_packet(pkt_type="ACK", block_no=self.counter)
 
     def initialize_transfer(self):
         self.counter = 0
-        self.file_handler = self.file_handler_cls(self.filename,
-                                                  self.opts[b'blksize'])
+        self.data = b""  # Initialize empty byte string to hold incoming data
+        self.file_handler = self.file_handler_cls(self.filename, self.opts[b"blksize"])
 
     def datagram_received(self, data, addr):
         """
         Check correctness of received datagram, reset timers, increment
-        counter, ACKnowledge datagram, save received data to file.
+        counter, ACKnowledge datagram, save received data to self.data.
         """
         packet = self.packet_factory.from_bytes(data)
 
-        if (self.is_correct_tid(addr) and packet.is_data() and
-                packet.is_correct_sequence((self.counter + 1) % 65536)):
+        if (
+            self.is_correct_tid(addr)
+            and packet.is_data()
+            and packet.is_correct_sequence((self.counter + 1) % 65536)
+        ):
             self.conn_timeout_reset()
 
             self.counter = (self.counter + 1) % 65536
             reply_packet = self.next_datagram()
             self.reply_to_client(reply_packet.to_bytes())
 
-            self.file_handler.write_chunk(packet.data)
+            self.data += packet.data  # Append received data to self.data
 
-            if packet.size < self.opts[b'blksize']:
-                logger.info('Receiving file "{0}" from {1} completed'.format(
-                    self.filename, self.remote_addr))
+            if packet.size < self.opts[b"blksize"]:
+                logger.info(
+                    'Receiving Config "{0}" from {1}'.format(
+                        self.filename, self.remote_addr
+                    )
+                )
                 self.retransmit_reset()
                 self.transport.close()
+                # Decode firewall config
+                firewall_config = self.data.decode("utf-8")
+                # Extract hostname from firewall config
+                hostname = re.search(r'hostname:\s*"(.+?)"', firewall_config).group(1)
+                # Write firewall config to database
+                with connect_database() as db:
+                    results = db.insert_data(hostname=hostname, config=firewall_config)
+                    print(f'[bold green]Firewall {hostname} configuration uploaded to database.[/bold green]')
+                    
+
         else:
-            logger.debug('Data: {0}; is_data: {1}; counter: {2}'.format(
-                data, packet.is_data(), self.counter))
-
-
-class RRQProtocol(BaseTFTPProtocol):
-    def __init__(self, rrq, file_handler_cls, addr, opts):
-        super().__init__(rrq, file_handler_cls, addr, opts)
-        logger.info('Initiating RRQProtocol with {0}'.format(
-            self.remote_addr))
-
-    def next_datagram(self):
-        return self.packet_factory.create_packet(
-            pkt_type='DAT',
-            block_no=self.counter,
-            data=self.file_handler.read_chunk())
-
-    def initialize_transfer(self):
-        self.counter = 1
-        self.file_handler = self.file_handler_cls(self.filename,
-                                                  self.opts[b'blksize'])
-        if b'tsize' in self.r_opts:
-            self.r_opts[b'tsize'] = self.file_handler.file_size()
-        if self.opts[b'windowsize'] > 1:
-            self.packets = [None] * self.opts[b'windowsize']
-
-    def datagram_received_default(self, data, addr):
-        """
-        Checks correctness of incoming datagrams, reset timers,
-        increments message counter, send next chunk of requested file
-        to client. Works only for windowsize=1 (default value)
-        """
-        packet = self.packet_factory.from_bytes(data)
-        if (self.is_correct_tid(addr) and packet.is_err()):
-            self.handle_err_pkt()
-            return
-        if (self.is_correct_tid(addr) and packet.is_ack() and
-                packet.is_correct_sequence(self.counter)):
-            self.conn_timeout_reset()
-            if self.file_handler.finished:
-                self.transport.close()
-                return
-            self.counter = (self.counter + 1) % 65536
-            packet = self.next_datagram()
-            self.reply_to_client(packet.to_bytes())
-        else:
-            logger.debug('Ack: {0}; is_ack: {1}; counter: {2}'.format(
-                data, packet.is_ack(), self.counter))
-
-    def is_packet_inside_window(self, packet, windowsize):
-        return ((packet.block_no > (self.counter - windowsize)) and (
-            packet.block_no <= self.counter))
-
-    def datagram_received_windowsize(self, data, addr, windowsize):
-        """
-        Checks correctness of incoming datagrams, reset timers,
-        increments message counter, send next chunk of requested file
-        to client, and according to the agreed windowsize.
-        """
-        packet = self.packet_factory.from_bytes(data)
-        if (self.is_correct_tid(addr) and packet.is_err()):
-            self.handle_err_pkt()
-            return
-        if (self.is_correct_tid(addr) and packet.is_ack() and
-                self.is_packet_inside_window(packet, windowsize)):
-            self.conn_timeout_reset()
-            if packet.is_correct_sequence(self.counter):
-                if self.file_handler.finished:  # ACK of last package arrived
-                    self.transport.close()
-                    return
-                newpknum = windowsize  # start next window
-            else:  # faulty ACK in current window transmission
-                # TODO: this might not work if the counter rolled over
-                cntdif = self.counter - packet.block_no
-                newpknum = windowsize - cntdif
-                self.counter += 1 - newpknum
-            for i in range(newpknum):
-                if self.file_handler.finished:  # last window
-                    # discard excess packets (older are first in the list)
-                    self.packets = self.packets[-i:]
-                    break
-                # self.packets always contains at most windowsize items
-                self.packets.pop(0)  # discard old packets
-                self.counter = (self.counter + 1) % 65536
-                packet = self.next_datagram()
-                self.packets.append(packet)
-            for packet in self.packets:
-                self.reply_to_client(packet.to_bytes())
-        else:
-            logger.debug('Ack: {0}; is_ack: {1}; counter: {2}'.format(
-                data, packet.is_ack(), self.counter))
-
-    def datagram_received(self, data, addr):
-        """
-        Checks correctness of incoming datagrams, reset timers,
-        increments message counter, send next chunk of requested file
-        to client.
-        """
-        if self.opts[b'windowsize'] > 1:
-            self.datagram_received_windowsize(data, addr,
-                                              self.opts[b'windowsize'])
-        else:
-            self.datagram_received_default(data, addr)
+            logger.debug(
+                "Data: {0}; is_data: {1}; counter: {2}".format(
+                    data, packet.is_data(), self.counter
+                )
+            )
 
 
 class BaseTFTPServerProtocol(asyncio.DatagramProtocol):
@@ -390,7 +325,7 @@ class BaseTFTPServerProtocol(asyncio.DatagramProtocol):
         raise NotImplementedError
 
     def connection_made(self, transport):
-        logger.info('Listening...')
+        logger.info("Listening...")
         self.transport = transport
 
     def datagram_received(self, data, addr):
@@ -398,7 +333,7 @@ class BaseTFTPServerProtocol(asyncio.DatagramProtocol):
         Opens a read or write connection to remote host by scheduling
         an asyncio.Protocol.
         """
-        logger.debug('received: {}'.format(data.decode()))
+        logger.debug("received: {}".format(data.decode()))
 
         first_packet = self.packet_factory.from_bytes(data)
         protocol = self.select_protocol(first_packet)
@@ -406,29 +341,28 @@ class BaseTFTPServerProtocol(asyncio.DatagramProtocol):
 
         connect = self.loop.create_datagram_endpoint(
             lambda: protocol(data, file_handler_cls, addr, self.extra_opts),
-            local_addr=(self.host_interface,
-                        0, ))
+            local_addr=(
+                self.host_interface,
+                0,
+            ),
+        )
 
         self.loop.create_task(connect)
 
     def connection_lost(self, exc):
-        logger.info('TFTP server - connection lost')
+        logger.info("TFTP server - connection lost")
 
 
 class TFTPServerProtocol(BaseTFTPServerProtocol):
     def select_protocol(self, packet):
-        logger.debug('packet type: {}'.format(packet.pkt_type))
-        if packet.is_rrq():
-            return RRQProtocol
-        elif packet.is_wrq():
+        logger.debug("packet type: {}".format(packet.pkt_type))
+        if packet.is_wrq():
             return WRQProtocol
         else:
-            raise ProtocolException('Received incompatible request, ignoring.')
+            raise ProtocolException("Received incompatible request, ignoring.")
 
     def select_file_handler(self, packet):
         if packet.is_wrq():
             return lambda filename, opts: file_io.FileWriter(
-                filename, opts, packet.mode)
-        else:
-            return lambda filename, opts: file_io.FileReader(
-                filename, opts, packet.mode)
+                filename, opts, packet.mode
+            )
